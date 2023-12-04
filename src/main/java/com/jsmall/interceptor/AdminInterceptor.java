e6292dd1e1dbc28b09d35c7cf8210935287e1201
choi2424 <choi242002@gmail.com>
2023-12-04 17:00:40 +0900

    인터셉터 기능 작업중

---
+++ src/main/java/com/jsmall/interceptor/AdminInterceptor.java
@@ -0,0 +1,86 @@
+package com.jsmall.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.jsmall.domain.AdminVO;
+
+
+
+public class AdminInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+				throws Exception {
+		
+		boolean result = false;
+		
+		// 현재 클라이언트의 세션을 통한 인증상태 확인작업을 할수가 있다.
+		HttpSession session = request.getSession();
+		
+		AdminVO user = (AdminVO) session.getAttribute("adminStatus");
+		
+		if(user == null) { // 인증정보가 없는경우
+			
+			result = false; // 컨트롤러로 진행을 하지 못한다
+			
+			if(isAjaxRequest(request)) {
+				
+				response.sendError(400); // 클라이언트의 ajax구문에서 error : 동작됨.
+			}else {
+				
+				getTargetUrl(request);
+				
+				response.sendRedirect("/admin/ad_login");
+				
+			}
+			
+			
+		}else { // 인증정보가 있는 경우 . 즉 세션 loginStatus 정보가 존재(사용자는 로그인중)
+			
+			result = true; // 컨트롤러로 진행을 하게된다.
+			
+		}
+		
+		return result;
+	}
+	
+	// 인증되지 않은 상태에서 사용자가 요청한 URL을 저장하고 로그인후 URL로 리다이렉트 작업
+	private void getTargetUrl(HttpServletRequest request) {
+		
+		// 예 요청주소 . /member/modify?userid=user01
+		
+		String uri = request.getRequestURI(); // /user/order/order_info
+		String query = request.getQueryString(); // userid=user01
+		
+		if(query == null || query.equals("null")) {
+			query = "";
+		}else {
+			query = "?" + query;
+		}
+		
+		String targetUrl = uri + query;
+		
+		if(request.getMethod().equals("GET")) {
+			request.getSession().setAttribute("targetUrl", targetUrl);
+		}
+		
+		
+	}
+	
+	// 사용자 요청이 ajax요청인지 체크
+	private boolean isAjaxRequest(HttpServletRequest request) {
+		
+		boolean isAjax = false;
+		
+		String header = request.getHeader("AJAX");
+		if(header != null && header.equals("true")) {
+			isAjax = true;
+		}
+		
+		return isAjax;
+	}
+}
